f98de625f0ed400ee61c718ec2854a314c6a2c2f
stevenwhy <stevenyildirim@gmail.com>
2022-03-25 18:52:45 -0400

    find missing number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,6 +13,37 @@ fun main() {
             "expected: [1, 2, 3, 4, 5, 6]\n" +
             "actual: ${sort(mutableListOf(2, 6, 4, 3, 1, 5))}")
 
+    println("Cyclic Sort #2 Find Missing Num -------------------")
+    println("Input: [4, 0, 3, 1]\n" +
+            "Output: 2\n" +
+            "actual: ${findMissingNumber(mutableListOf(4, 0, 3, 1))}")
+    println("Input: [8, 3, 5, 2, 4, 6, 0, 1]\n" +
+            "Output: 7\n" +
+            "actual: ${findMissingNumber(mutableListOf(8, 3, 5, 2, 4, 6, 0, 1))}")
+
+}
+/*
+    We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’.
+    Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
+ */
+fun findMissingNumber(list: MutableList<Int>): Int {
+    var index = 0
+    val size = list.size
+    while(index < size) {
+        val num = list[index]
+        if(num < size && num != index) {
+            //swap
+            list[index] = list[num]
+            list[num] = num
+        } else {
+            index++
+        }
+    }
+
+    for(i in list.indices) {
+        if(i != list[i]) return i
+    }
+    return size
 }
 
 fun sort(list: MutableList<Int>): MutableList<Int> {
